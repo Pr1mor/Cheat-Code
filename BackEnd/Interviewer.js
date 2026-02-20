7111f5877a1c031f6a362c3fdc02bbe53270dfa5
Zeel Modi <modizs@myumanitoba.ca>
2026-02-20 15:46:47 -0600

    Added Brain of AI

---
+++ BackEnd/Interviewer.js
@@ -0,0 +1,11 @@
+import { GoogleGenAI } from "@google/genai";  // Importing the AI Library
+
+//Getting our AI brain to start working over here
+const interviewer = new GoogleGenAI({});
+
+async function main() {
+    const chats = interviewer.chats.create({
+        
+    })
+}
+
