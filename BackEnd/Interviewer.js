f3483af336c9adc57f01be4a3ca5e0d7c8f206fd
Zeel Modi <modizs@myumanitoba.ca>
2026-02-20 18:45:23 -0600

    Worked on prompts for gemini

--- BackEnd/Interviewer.js
+++ BackEnd/Interviewer.js
@@ -1,10 +1,34 @@
 require('dotenv').config();
+
+const express = require("express"); // Setting up express js
+const cors = require("cors");
 const { GoogleGenAI } = require("@google/genai");  // Importing the AI Library
+const app = express();
+
+// Making sure that the express variable can parse json questions and be able to easily open up in the localhost
+app.use(express.json());
+app.use(cors(
+    {
+        origin: "https://localhost:3000"
+    }
+))
 
 //Getting our AI brain to start working over here
 const interviewer = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
-async function main() {
+//All of the chats will be stored over here
+const currentHistory = {};
+
+const GEMINI_PROMPT = `You are a strict senior FAANG software engineer conducting a technical mock interview. The candidate is solving a Data Structures and Algorithm Problem.
+                        YOUR STRICT RULES:
+                        1. NEVER give the candidate the raw code or direct answer under any circumstances.
+                        2. Evaluate their logic and pseudocode. If they suggest a Brute Force approach, ask them to optimize it.
+                        3. Constantly ask for the Time (Big O) and Space complexity of their proposed solution.
+                        4. Keep your responses under 3 sentences to mimic natural speech.`;
+
+app.post("/chats", async (req, res) => {
+    const{PROMPT} = req.body;
+
     const chats = interviewer.chats.create({
         model: "gemini-2.5-flash",
         history: [
@@ -19,15 +43,4 @@ async function main() {
         ],       
     });
 
-    const firstResponse = await chats.sendMessage({
-        message: "I have 2 dogs in my house."
-    });
-    console.log("Chat Response 1:", firstResponse.text);
-
-    const secondResponse = await chats.sendMessage({
-        message: "How many paws are in my house?",
-    });
-    console.log("Chat Response 2:", secondResponse.text);
-}
-
-main();
+})
